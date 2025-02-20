70d7662798429549a826cdbc7e8b637f5bf4241a
Igor Mokhov <igormokhovid@gmail.com>
2025-02-20 14:58:53 +0700

    feat: added Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { GlobalStyles } from './GlobalStyles';
+import { Container } from './UI/Container/Container';
 
 export const App = () => {
   return (
     <>
       <GlobalStyles />
-      <h1>Weather App</h1>
+      <Container>
+        <h1>Weather App</h1>
+      </Container>
     </>
   );
 };
---
+++ src/UI/Container/Container.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+const Wrapper = styled.main`
+  width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+
+export const Container = ({ children }: ContainerProps) => {
+  return <Wrapper>{children}</Wrapper>;
+};
