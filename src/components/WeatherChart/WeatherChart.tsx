5858de0791db9b37ea9b14fbc34c4bb8497858c3
Igor Mokhov <igormokhovid@gmail.com>
2025-02-22 15:57:39 +0700

    feat: added the ability to select one of the filters

--- src/components/WeatherChart/WeatherChart.tsx
+++ src/components/WeatherChart/WeatherChart.tsx
@@ -7,7 +7,7 @@ import {
   YAxis,
 } from 'recharts';
 import styled from 'styled-components';
-import { IWeatherForecast } from '../../types/weather';
+import { IWeatherForecast, WeatherFilters } from '../../types/weather';
 import { processWeatherData } from '../../utils/weatherUtils';
 
 const StyledChart = styled.div`
@@ -18,9 +18,10 @@ const StyledChart = styled.div`
 
 interface IWeatherChartProps {
   weather: IWeatherForecast | null;
+  activeFilter: WeatherFilters;
 }
 
-export const WeatherChart = ({ weather }: IWeatherChartProps) => {
+export const WeatherChart = ({ weather, activeFilter }: IWeatherChartProps) => {
   const chartData = processWeatherData(weather);
 
   if (!weather) return null;
@@ -29,21 +30,21 @@ export const WeatherChart = ({ weather }: IWeatherChartProps) => {
     <StyledChart>
       <AreaChart width={1200} height={350} data={chartData.list}>
         <defs>
-          <linearGradient id="colorTemp" x1="0" y1="0" x2="0" y2="1">
+          <linearGradient id="colorData" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
             <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
           </linearGradient>
         </defs>
         <XAxis dataKey="time" />
         <YAxis domain={['auto', 'auto']} />
-        <CartesianGrid strokeDasharray="3 3" />
+        <CartesianGrid strokeDasharray="1" />
         <Tooltip />
         <Area
           type="monotone"
-          dataKey="temp"
+          dataKey={activeFilter}
           stroke="#8884d8"
           fillOpacity={1}
-          fill="url(#colorTemp)"
+          fill="url(#colorData)"
         />
       </AreaChart>
     </StyledChart>
--- src/components/WeatherWidget/WeatherWidget.tsx
+++ src/components/WeatherWidget/WeatherWidget.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import { Form } from '../Form/Form';
 import { useState } from 'react';
-import { IWeatherForecast } from '../../types/weather';
+import { IWeatherForecast, WeatherFilters } from '../../types/weather';
 import { WeatherChart } from '../WeatherChart/WeatherChart';
+import { WeatherControls } from '../WeatherControls/WeatherControls';
 
 const StyledSection = styled.section`
   margin-bottom: 50px;
@@ -14,15 +15,30 @@ const StyledTitle = styled.h2`
   font-weight: 200;
 `;
 
+const WeatherHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 export const WeatherWidget = () => {
+  const [activeFilter, setActiveFilter] = useState<WeatherFilters>('temp');
   const [weatherForecast, setWeatherForecast] =
     useState<IWeatherForecast | null>(null);
 
   return (
     <StyledSection>
       <Form saveWeatherForecast={setWeatherForecast} />
-      <StyledTitle>{weatherForecast?.city.name}</StyledTitle>
-      <WeatherChart weather={weatherForecast} />
+      {weatherForecast && (
+        <WeatherHeader>
+          <StyledTitle>{weatherForecast?.city.name}</StyledTitle>
+          <WeatherControls
+            activeFilter={activeFilter}
+            setActiveFilter={setActiveFilter}
+          />
+        </WeatherHeader>
+      )}
+      <WeatherChart weather={weatherForecast} activeFilter={activeFilter} />
     </StyledSection>
   );
 };
