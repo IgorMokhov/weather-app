d5816d1001928871e38619b8fff5f05d108faf3f
Igor Mokhov <igormokhovid@gmail.com>
2025-02-25 18:36:12 +0700

    fix: Rechart was changed to Highcharts

--- src/components/WeatherChart/WeatherChart.tsx
+++ src/components/WeatherChart/WeatherChart.tsx
@@ -1,52 +1,70 @@
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts';
 import styled from 'styled-components';
-import { IWeatherForecast, WeatherFilters } from '../../types/weather';
-import { processWeatherData } from '../../utils/weatherUtils';
+import { IChartWeatherData, WeatherFilters } from '../../types/weather';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts, { SeriesOptionsType } from 'highcharts';
 
 const StyledChart = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  margin: 35px 0;
+  border-radius: 10px;
 `;
 
 interface IWeatherChartProps {
-  weather: IWeatherForecast | null;
+  weatherList: IChartWeatherData[];
   activeFilter: WeatherFilters;
 }
 
-export const WeatherChart = ({ weather, activeFilter }: IWeatherChartProps) => {
-  const chartData = processWeatherData(weather);
+export const WeatherChart = ({
+  weatherList,
+  activeFilter,
+}: IWeatherChartProps) => {
+  const processDataForHighcharts = (weatherList: IChartWeatherData[]) => {
+    return weatherList.map(
+      (weather) =>
+        ({
+          name: weather.city,
+          data: weather.list.map((item) => [item.time, item[activeFilter]]),
+          type: 'spline',
+        } as SeriesOptionsType)
+    );
+  };
 
-  if (!weather) return null;
+  const series = processDataForHighcharts(weatherList);
+
+  const options: Highcharts.Options = {
+    chart: {
+      type: 'spline',
+      backgroundColor: 'var(--bg-color)',
+      height: 350,
+      width: 1200,
+      style: {
+        borderRadius: 25,
+      },
+    },
+    title: {
+      text: '',
+    },
+    xAxis: {
+      type: 'datetime',
+      title: {
+        text: 'Date',
+      },
+    },
+    yAxis: {
+      title: {
+        text: activeFilter,
+      },
+    },
+    tooltip: {
+      xDateFormat: '%Y-%m-%d',
+    },
+    series: series,
+  };
+
+  if (!weatherList?.length) return null;
 
   return (
     <StyledChart>
-      <AreaChart width={1200} height={350} data={chartData.list}>
-        <defs>
-          <linearGradient id="colorData" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-          </linearGradient>
-        </defs>
-        <XAxis dataKey="time" />
-        <YAxis domain={['auto', 'auto']} />
-        <CartesianGrid strokeDasharray="1" />
-        <Tooltip />
-        <Area
-          type="monotone"
-          dataKey={activeFilter}
-          stroke="#8884d8"
-          fillOpacity={1}
-          fill="url(#colorData)"
-        />
-      </AreaChart>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </StyledChart>
   );
 };
