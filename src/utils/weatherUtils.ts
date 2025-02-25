34aa1c543f1365c6fc2c1fbee82eefc0f9a36a8d
Igor Mokhov <igormokhovid@gmail.com>
2025-02-25 18:31:35 +0700

    feat: updated processWeatherData for working with arrays

--- src/utils/weatherUtils.ts
+++ src/utils/weatherUtils.ts
@@ -5,10 +5,8 @@ import {
 } from '../types/weather';
 
 export const filterDailyForecast = (
-  weatherData: IWeatherForecast | null
-): IWeatherForecast | null => {
-  if (!weatherData) return null;
-
+  weatherData: IWeatherForecast
+): IWeatherForecast => {
   const filteredList = weatherData.list.reduce<IWeatherData[]>((acc, entry) => {
     const date = entry.dt_txt.split(' ')[0];
     const isNewNoonForecast =
@@ -28,20 +26,18 @@ export const filterDailyForecast = (
   };
 };
 
-export const processWeatherData = (
-  weather: IWeatherForecast | null
-): IChartWeatherData => {
-  if (!weather) return { city: '', list: [] };
-
-  return {
+export const extractWeatherData = (
+  weatherList: IWeatherForecast[]
+): IChartWeatherData[] => {
+  return weatherList?.map((weather) => ({
     city: weather?.city.name,
     list: weather?.list.map(({ main, wind, dt_txt }) => ({
-      time: dt_txt.split(' ')[0],
+      time: dt_txt,
       temp: main.temp,
       humidity: main.humidity,
       pressure: main.pressure,
       windTemp: wind.deg,
       windSpeed: wind.speed,
     })),
-  };
+  }));
 };
